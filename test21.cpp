5d06223e7bd9b8476a9278610d2d120c1d61bf9a
Raphael Isemann <risemann@apple.com>
2019-07-25 14:47:07 +0200

    New test that works with c++11

--- test21.cpp
+++ test21.cpp
@@ -1,15 +1,44 @@
-#include <bit>
-#include <bitset>
-#include <cstdint>
 #include <iostream>
+#include <memory>
+#include <utility>
+ 
+struct A {
+    A(int&& n) { std::cout << "rvalue overload, n=" << n << "\n"; }
+    A(int& n)  { std::cout << "lvalue overload, n=" << n << "\n"; }
+};
+ 
+class B {
+public:
+    template<class T1, class T2, class T3>
+    B(T1&& t1, T2&& t2, T3&& t3) :
+        a1_{std::forward<T1>(t1)},
+        a2_{std::forward<T2>(t2)},
+        a3_{std::forward<T3>(t3)}
+    {
+    }
+ 
+private:
+    A a1_, a2_, a3_;
+};
+ 
+template<class T, class U>
+std::unique_ptr<T> make_unique1(U&& u)
+{
+    return std::unique_ptr<T>(new T(std::forward<U>(u)));
+}
+ 
+template<class T, class... U>
+std::unique_ptr<T> make_unique2(U&&... u)
+{
+    return std::unique_ptr<T>(new T(std::forward<U>(u)...));
+}
  
 int main()
-{
-    std::uint8_t i = 0b00011101;
-    std::cout << "i          = " << std::bitset<8>(i) << '\n';
-    std::cout << "rotl(i,0)  = " << std::bitset<8>(std::rotl(i,0)) << '\n';
-    std::cout << "rotl(i,1)  = " << std::bitset<8>(std::rotl(i,1)) << '\n';
-    std::cout << "rotl(i,4)  = " << std::bitset<8>(std::rotl(i,4)) << '\n';
-    std::cout << "rotl(i,9)  = " << std::bitset<8>(std::rotl(i,9)) << '\n';
-    std::cout << "rotl(i,-1) = " << std::bitset<8>(std::rotl(i,-1)) << '\n';
+{   
+    auto p1 = make_unique1<A>(2); // rvalue
+    int i = 1;
+    auto p2 = make_unique1<A>(i); // lvalue
+ 
+    std::cout << "B\n";
+    auto t = make_unique2<B>(2, i, 3);
 }
